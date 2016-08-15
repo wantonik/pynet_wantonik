3c92bb079914a451a4bee4489cb3a47ab0c60ac9
Waldemar Antonik <waldemar.antonik@yahoo.ie>
2016-08-15 21:25:32 +0100

    Submitting e1c excercise for Python library/module path

---
+++ class2/e1c_my_func.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+## Simple script which prints 'hello' and then can be used in another program
+
+def hello_func():
+    print 'hello'
+
+# Invoke hello_func function
+hello_func()
---
+++ class2/e1c_test_my_func.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+## This script imports my_func function from e1c_my_func.py module
+import e1c_my_func
+
+print e1c_my_func.hello_func
